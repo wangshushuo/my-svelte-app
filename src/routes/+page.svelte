5f2c821e75c77c1620d0638930864de1fe0ec837
wangshushuo <wangshushuo@qq.com>
2023-12-09 10:18:35 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { onMount } from 'svelte';
+	import { writable } from "svelte/store";
+
+	/** @type { import("svelte/store").Writable<any[]> } */
+	let tasks = writable( []);
+
+	onMount(() => {
+		tasks.set([
+			{ id: 1, name: "Task 1" },
+		])
+	});
+</script>
+
+<ul>
+	{#each $tasks as task}
+		<li>{task.name}</li>
+	{/each}
+</ul>
+<button on:click={() => tasks.update(t => [...t, { id: 2, name: "Task 2" }])}>Add</button>
